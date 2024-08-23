42098e5e83833277c1037433c85e2a6228eb334a
Zan Fras <zan.fraas@gmail.com>
2024-08-23 09:39:44 +0200

    reproduce

--- plugins/http.ts
+++ plugins/http.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { stringify } from 'qs'
 
 export default defineNuxtPlugin({
   name: 'http',
@@ -11,34 +10,6 @@ export default defineNuxtPlugin({
         Accept: 'application/json',
       },
     })
-
-    http.interceptors.request.use(
-      (config) => {
-        config.paramsSerializer = {
-          serialize: (params) =>
-            stringify(params, {
-              arrayFormat: 'brackets',
-              encode: false,
-              skipNulls: true,
-              filter: (_, value) => value || undefined,
-            }),
-        }
-
-        return config
-      },
-      (error) => {
-        return Promise.reject(error)
-      },
-    )
-
-    http.interceptors.response.use(
-      (response) => {
-        return response
-      },
-      async (error) => {
-        return Promise.reject(error)
-      },
-    )
 
     return {
       provide: {
